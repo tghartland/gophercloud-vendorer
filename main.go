70d052b51a502daa01feb88d4e9583fc2b06eff5
Thomas Hartland <thomas.george.hartland@cern.ch>
2019-11-19 15:30:07 +0100

    Add nodegroups and stacks

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	_ "github.com/gophercloud/gophercloud/openstack"
 	_ "github.com/gophercloud/gophercloud/openstack/containerinfra/apiversions"
 	_ "github.com/gophercloud/gophercloud/openstack/containerinfra/v1/clusters"
+	_ "github.com/gophercloud/gophercloud/openstack/containerinfra/v1/nodegroups"
 	_ "github.com/gophercloud/gophercloud/openstack/identity/v3/extensions/trusts"
+	_ "github.com/gophercloud/gophercloud/openstack/orchestration/v1/stackresources"
+	_ "github.com/gophercloud/gophercloud/openstack/orchestration/v1/stacks"
 	_ "github.com/gophercloud/gophercloud/testhelper"
 )
 
